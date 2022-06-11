5c22a5e4f88c0d196ce5e8e9e94f020f701a7951
silvanet <silvanet@gmail.com>
2022-06-11 13:35:21 -0400

    Welcome & Bye for now

--- pages/home.js
+++ pages/home.js
@@ -27,9 +27,16 @@ export default function Home({ initialTweets}) {
 
   return (
     <>
+      <p className='border px-8 py2 mt-2 font-bold'>Welcome - {session.user.name}</p>
       <NewTweet />
       <Tweets tweets={tweets} />
       <LoadMore tweets={tweets} setTweets={setTweets} />
+      <a
+          className='border px-8 py-2 mt-5 font-bold rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker'
+          href='/api/auth/signout'
+        >
+          Bye for now
+        </a>
     </>
   )
 }
@@ -44,3 +51,8 @@ export async function getServerSideProps() {
     },
   }
 }
+
+// sarah also added this here
+// if (session && !session.user.name) {
+//   router.push('/setup')
+// }
