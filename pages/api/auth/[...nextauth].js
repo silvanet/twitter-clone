1be4f7ec53234e0f70c51cf354e61e4df405d235
silvanet <silvanet@gmail.com>
2022-07-02 09:56:53 -0400

    Push to update email provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,26 +4,26 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from 'lib/prisma'
 
 export default NextAuth({
-  // providers: [
-  //   EmailProvider({
-  //     server: process.env.EMAIL_SERVER,
-  //     from: process.env.EMAIL_FROM
-  //   })
-  // ],
-
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
+    })
   ],
+
+  // providers: [
+  //   EmailProvider({
+  //     server: {
+  //       host: process.env.EMAIL_SERVER_HOST,
+  //       port: process.env.EMAIL_SERVER_PORT,
+  //       auth: {
+  //         user: process.env.EMAIL_SERVER_USER,
+  //         pass: process.env.EMAIL_SERVER_PASSWORD,
+  //       },
+  //     },
+  //     from: process.env.EMAIL_FROM,
+  //   }),
+  // ],
 
   database: process.env.DATABASE_URL,
   secret: process.env.SECRET,
